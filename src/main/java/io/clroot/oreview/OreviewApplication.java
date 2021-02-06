39e04cfc3a6a7a8f9be3a1a7f9b506d2248d7730
clroot <abcdkh1209@gmail.com>
2021-02-06 11:08:47 +0900

    Init Project

---
+++ src/main/java/io/clroot/oreview/OreviewApplication.java
@@ -0,0 +1,13 @@
+package io.clroot.oreview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OreviewApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(OreviewApplication.class, args);
+  }
+
+}
---
+++ src/test/java/io/clroot/oreview/OreviewApplicationTests.java
@@ -0,0 +1,13 @@
+package io.clroot.oreview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OreviewApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
