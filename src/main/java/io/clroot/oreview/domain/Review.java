eb0c5e1794beb58239f71ba25607585845b18391
clroot <abcdkh1209@gmail.com>
2021-02-20 16:24:12 +0900

    formatting Review.java

--- src/main/java/io/clroot/oreview/domain/Review.java
+++ src/main/java/io/clroot/oreview/domain/Review.java
@@ -1,18 +1,11 @@
 package io.clroot.oreview.domain;
 
-import java.time.LocalDate;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDate;
 
 @Entity
 @NoArgsConstructor
